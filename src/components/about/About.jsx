37033c7d311f7db4df579b37035a225410dfc948
coder_ronnie <royalronnie05@gmail.com>
2025-03-22 23:08:49 +0530

    added icons to aboutpage

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,33 +5,45 @@ export const About = () => {
   return (
     <div>
       <div className="aboutCont">
-        <div className="aboutPage">
-          <h1>Welcome to BlueCross UK – Your Trusted Health Partner</h1>
-          <p>
-            At <b>BlueCross UK</b>, we believe that{" "}
-            <b> quality healthcare should be accessible to everyone</b>. As a
-            leading health insurance provider in the UK, we offer{" "}
-            <strong> affordable, flexible, and comprehensive coverage </strong>{" "}
-            designed to protect you and your loved ones. Whether you're an
-            individual, a family, or a self-employed professional, our tailored
-            insurance plans ensure that you receive the best medical care
-            without financial stress.
-          </p>
-          <p>
-            With a commitment to transparency, customer satisfaction, and
-            hassle-free claims, we make health insurance simple and
-            reliable—because your well-being is our priority.
-          </p>
-        </div>
+        <section>
+          <div className="aboutHeading">
+            <div>
+              <h2>OUR MISSION</h2>
+              <p className="misionPara">
+                At BlueCross, we are committed to simplifying lives by providing
+                efficient, cost-effective, and hassle-free insurance solutions,
+                saving our customers time, effort, and money.
+              </p>
+            </div>
+            <div>
+              <p>
+                At BlueCross, we empower you to make informed decisions with
+                confidence when selecting essential insurance solutions. With
+                BlueCross, you can conveniently compare policies online or
+                consult with our knowledgeable insurance experts over the phone.
+                If you choose to call, you'll speak with a dedicated UK-based
+                professional ready to assist you.
+              </p>
+
+              <p>
+                While our team operates across the UK, our headquarters ensure
+                we stay connected with the communities we serve. Whether
+                discussing the latest market trends or helping you find the best
+                coverage for your Health, Home, or Business Insurance, we are
+                here to provide reliable guidance every step of the way.
+              </p>
+            </div>
+          </div>
+        </section>
+
         <section className="compresive">
-          <p>
-            At <b>BlueCross UK,</b> we are committed to providing{" "}
-            <b> affordable, reliable, and comprehensive health insurance </b>{" "}
-            that gives you peace of mind. Our goal is to ensure that everyone
-            has access to <b> quality healthcare without financial burdens. </b>
-          </p>
           <div className="threeColumGrid">
             <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU1pgP8kKypy_E9XRa7aU4S8dOuMGXfp5UQg&s"
+                alt=""
+                width="64"
+              />
               <h4>Affordable Plans</h4>
               <p>
                 We offer flexible coverage options to suit different budgets,
@@ -40,6 +52,11 @@ export const About = () => {
               </p>
             </div>
             <div>
+              <img
+                src="https://static.thenounproject.com/png/3777058-200.png"
+                alt=""
+                width="64"
+              />
               <h4>Reliable Support</h4>
               <p>
                 Our dedicated customer service team is available 24/7 to assist
@@ -47,6 +64,11 @@ export const About = () => {
               </p>
             </div>
             <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCVsxcrMptpbUeJWzmB_FLZy0Zeh-l1JRnVA&s"
+                alt=""
+                width="64"
+              />
               <h4>Comprehensive Coverage </h4>
               <p>
                 From routine check-ups to specialized treatments, our plans
@@ -55,19 +77,14 @@ export const About = () => {
               </p>
             </div>
           </div>
-          <p className="mt-3">
-            At BlueCross UK, we believe in making health insurance simple,
-            transparent, and stress-free—because your well-being matters.
-          </p>
 
-          <p>
-            At <b>BlueCross UK</b>, your <b> health and satisfaction </b> are at
-            the heart of everything we do. We are dedicated to providing{" "}
-            <b>seamless, customer-centric insurance solutions </b> that put your
-            well-being first.
-          </p>
           <div className="threeColumGrid">
             <div>
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/8743/8743903.png"
+                alt=""
+                width="65"
+              />
               <h4>Customer Satisfaction First </h4>
               <p>
                 We listen to your needs and continuously improve our services to
@@ -75,6 +92,11 @@ export const About = () => {
               </p>
             </div>
             <div>
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/16347/16347261.png"
+                alt=""
+                width="65"
+              />
               <h4>Fast & Easy Claims </h4>
               <p>
                 Our efficient claims process ensures you get the support you
@@ -82,6 +104,11 @@ export const About = () => {
               </p>
             </div>
             <div>
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/4230/4230869.png"
+                alt=""
+                width="64"
+              />
               <h4>24/7 Assistance </h4>
               <p>
                 Our expert support team is always available to guide you through
@@ -89,11 +116,6 @@ export const About = () => {
               </p>
             </div>
           </div>
-          <p>
-            With trust, transparency, and care at our core, we make sure that
-            you feel supported every step of the way—because your peace of mind
-            is our priority.
-          </p>
         </section>
       </div>
     </div>
