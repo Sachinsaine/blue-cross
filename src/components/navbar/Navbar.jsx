b5ab3be9a7bd9895bd1847f6378f44aac3e9f448
coder_ronnie <royalronnie05@gmail.com>
2025-04-02 22:52:14 +0530

    navbar mobile-optimization done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,7 +31,7 @@ export const Navbar = () => {
               <p>Loading logo...</p>
             )}
           </Link>
-          <div>
+          <div className="windowLinks">
             <ul className="navList">
               <li className="nav-item">
                 <Link
@@ -64,6 +64,80 @@ export const Navbar = () => {
                 </Link>
               </li>
             </ul>
+          </div>
+          <div className="rightToggle">
+            <button
+              class="btn p-0"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasRight"
+              aria-controls="offcanvasRight"
+            >
+              <i class="bi bi-list fs-1"></i>
+            </button>
+
+            <div
+              class="offcanvas offcanvas-end"
+              tabindex="-1"
+              id="offcanvasRight"
+              aria-labelledby="offcanvasRightLabel"
+            >
+              <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">
+                  <Link to="/">
+                    {logo ? (
+                      <img
+                        src={logo}
+                        alt="BlueCross Logo"
+                        style={{ width: "160px", height: "auto" }}
+                      />
+                    ) : (
+                      <p>Loading logo...</p>
+                    )}
+                  </Link>
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="offcanvas-body">
+                <ul className="navList">
+                  <li className="nav-item">
+                    <Link
+                      to="/"
+                      className={`nav-link ${
+                        location.pathname === "/" ? "active" : ""
+                      }`}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/about"
+                      className={`nav-link ${
+                        location.pathname === "/about" ? "active" : ""
+                      }`}
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/contact"
+                      className={`nav-link ${
+                        location.pathname === "/contact" ? "active" : ""
+                      }`}
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
